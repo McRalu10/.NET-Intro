92422fc2fafce059f0bab5ed9ae18f04badd3878
Dragomir Alecsandra <dragomir.alecsandra19@gmail.com>
2021-10-01 17:53:50 +0300

    arranged code

---
+++ Lab1/Ex1/ManagerClass.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace ManagerClass
+{
+    public class Manager
+    {
+        protected Guid Id { get;}
+        protected string FirstName { get; set;}
+        protected string LastName { get; set;}
+        protected DateTime StartDate { get; set;}
+        protected DateTime EndDate { get; set;}
+        protected double Salary { get; set;}
+
+        public Guid _Id {
+            get => Id;
+        }
+
+        public string _FirstName {
+            get => FirstName;
+            set => FirstName = value;
+        }
+
+        public string _LastName {
+            get => LastName;
+            set => LastName = value;
+        }
+
+        public DateTime _StartDate {
+            get => StartDate;
+            set => StartDate = value;
+        }
+
+        public DateTime _EndDate {
+            get => EndDate;
+            set => EndDate = value;
+        }
+
+        public double _Salary {
+            get => Salary;
+            set => Salary = value;
+        }
+
+        public Manager(string firstName, string lastName, DateTime startDate, double salary) {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.StartDate = startDate;
+            this.Salary = salary;
+        }
+
+        public Manager(string firstName, string lastName) {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string GetFullName() {
+            string FullName = this.FirstName + " " + this.LastName;
+            return FullName;
+        }
+
+        public bool IsActive(DateTime CurrentDate) {
+            if(DateTime.Compare(this.EndDate, CurrentDate) > 0 && DateTime.Compare(CurrentDate, this.StartDate) > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
