da1fd75315e79dddfb1c7f9f541ca7b0bf1522ae
Raluca Macovei <ralumacovei@yahoo.com>
2021-10-01 16:11:26 +0300

    initial commit

---
+++ Lab1/Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        public class Employee {
+
+            protected Guid Id { get; set;}
+            public string FirstName { get;}
+
+            public Employee(string name){
+                this.FirstName =  name;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Ion");
+            Console.WriteLine("Hello" + employee.FirstName + "!");
+        }
+    }
+}
