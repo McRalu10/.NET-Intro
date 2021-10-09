75d9dd4d1665a4f7b7d500e8ba7b20781e22291d
Dragomir Alecsandra <dragomir.alecsandra19@gmail.com>
2021-10-09 18:07:33 +0300

    implemented reading from .dat file

--- Lab2/Ex1/Program.cs
+++ Lab2/Ex1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Ex1
 {
@@ -6,7 +8,13 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader objInput = new StreamReader("G:\\Facultate\\Anul III - Sem I\\I .NET\\Lab\\.NET-Intro\\Lab2\\Ex1\\weather.dat",  System.Text.Encoding.Default);
+            string contents = objInput.ReadToEnd().Trim();
+            string [] split = System.Text.RegularExpressions.Regex.Split(contents, "\\s+", RegexOptions.None);
+            foreach (string s in split)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
